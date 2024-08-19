36b58127d1322c9f7508ed399955bea9b8ab26b0
barangievans <barangievans@gmail.com>
2024-08-19 03:37:08 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import About from "./components/About";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
+  const login = () => setIsLoggedIn(true);
+  const logout = () => setIsLoggedIn(false);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, [isLoggedIn, navigate]);
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      {/* Conditional rendering to show NavBar if logged in, otherwise navigate to login */}
+      {isLoggedIn ? <NavBar logout={logout} /> : <Navigate to="/login" />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Outlet context={login} />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,7 @@
-
+import React from 'react';
 
 function Home() {
-  return (
-    <main>
-      <h1>Home!</h1>
-    </main>
-  );
+  return <h1>Home Page</h1>;
 }
 
 export default Home;
